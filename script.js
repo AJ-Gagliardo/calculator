4a3b7a948d06a5d0568c861ef4b62785efe8b251
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-28 19:17:25 -0800

    added a way to show in the calculator the number that is being pressed, currently working with number 1, i will ad it to the other numbers and start testing to see if tehre are any bugs

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 
+let displayResult = (result)=> displayBot.textContent = result;
+
 const add=(a,b)=>{
-return a + b;
+    let result = a + b;
+    displayResult(result);
+    return result; 
 };
 
 const subsctract = (a,b)=> {
+    let result = a-b;
+    displayResult(result);
     return a-b;
 };
 
 const multiply = (a,b)=>{
+    let result = a*b;
+    displayResult(result);
     return a*b;
 }
 
 const divide = (a,b)=>{
+    let result = a/b;
+    displayResult(result);
     return a/b;
 }
 
 
 // variables for operation
 
-let n1 = 1; //first number on the operation
-let operator = '+'; //if its plus, minus, etc.
-let n2 = 2; //second number on the operation
+let n1 ; //first number on the operation
+let operator ; //if its plus, minus, etc.
+let n2 ; //second number on the operation
 
 // const operate = ()=>{
 // if(operator == '+') return add(n1,n2)
@@ -34,4 +44,31 @@ return (operator == '+') ? add(n1,n2)
 : 'n/a'
 }
 
-const display = document.getElementsByClassName('display');
+
+let displayPrevious = [n1,operator,n2];
+// let displayValue;
+
+
+// DOM
+const displayBot = document.getElementById('display-bot');
+const displayTop = document.getElementById('display-top');
+
+
+
+let displayNumber = [];
+
+const num1= document.getElementById('num1');
+
+num1.addEventListener('click', ()=>{
+    displayNumber.push(1);
+    displayBot.textContent = displayNumber.join('');
+})
+
+
+
+
+
+
+
+
+
