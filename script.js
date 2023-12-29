06eba068b8155a5c7f269938d3bdad292917324d
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-28 17:12:40 -0800

    added an operator function to check if the operation is a sum, it will automatically add (will improve this, so far is working with adding)

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ const multiply = (a,b)=>{
 const divide = (a,b)=>{
     return a/b;
 }
+
+
+// variables for operation
+
+let n1 = 1; //first number on the operation
+let operator = '+'; //if its plus, minus, etc.
+let n2 = 2; //second number on the operation
+
+// const operate = ()=>{
+// if(operator == '+') return add(n1,n2)
+// }
+
+const operate = ()=>{
+return (operator == '+') ? add(n1,n2): 'n/a'
+}
